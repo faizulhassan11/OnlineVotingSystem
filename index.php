ab40a61c0a4b30b9ebfd6389c2eba1e35a2d9546
Faiz Ul Hassan <faizulhassansokhal@gmail.com>
2024-07-22 11:45:22 +0500

    Resolve merge conflicts

--- index.php
+++ index.php
@@ -68,7 +68,6 @@
         integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
 </head>
 
-
 <body>
     <div class="container h-100">
         <div class="d-flex justify-content-center h-100">
